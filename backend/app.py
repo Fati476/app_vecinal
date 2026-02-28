4a940d0546e67fb602deab5930049c9f28cd009f
fati1 <fm2290759@gmail.com>
2026-02-28 15:17:03 -0600

    fix correo render

--- backend/app.py
+++ backend/app.py
@@ -38,8 +38,8 @@ print("API KEY:", API_KEY)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'fm2290759@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'lqca vjtr jvrk wlkt'
+app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = 'ConectaVecinos <fm2290759@gmail.com>'
 mail = Mail(app)
 
